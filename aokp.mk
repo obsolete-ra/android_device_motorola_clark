c1660c01d62876cfcd6065e7c9dda68f2000fe46
rob e anderson <obsolete.ra@gmail.com>
2017-07-21 10:24:07 -0400

    AOKP-ify

--- aokp.mk
+++ aokp.mk
@@ -5,12 +5,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_clark
+PRODUCT_NAME := aokp_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
